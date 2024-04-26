51de3613fa9268a8fdfc8264a883acc5b35aa552
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-04-26 11:04:29 +0200

    add edge filter, stub initial filter, notebook

--- twm_proj/implementation/edge_filter.py
+++ twm_proj/implementation/edge_filter.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 from twm_proj.interface.edge_filter import IEdgeFilter
@@ -5,4 +6,6 @@ from twm_proj.interface.edge_filter import IEdgeFilter
 
 class EdgeFilter(IEdgeFilter):
     def filter(self, image: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 100, 200)
+        return edges
--- twm_proj/implementation/initial_filter.py
+++ twm_proj/implementation/initial_filter.py
@@ -5,4 +5,4 @@ from twm_proj.interface.initial_filter import IInitialFilter
 
 class InitialFilter(IInitialFilter):
     def filter(self, image: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        return image
