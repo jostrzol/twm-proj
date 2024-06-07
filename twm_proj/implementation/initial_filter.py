5abd9997076a4abc1be5e8423fa706f41bff0406
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-06-07 17:50:42 +0200

    style

--- twm_proj/implementation/initial_filter.py
+++ twm_proj/implementation/initial_filter.py
@@ -6,12 +6,8 @@ from twm_proj.interface.initial_filter import IInitialFilter
 
 class InitialFilter(IInitialFilter):
     def filter(self, image: np.ndarray) -> np.ndarray:
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        saturated = self.color_filter(hsv, (100, 135), saturate=0.5, brighten=1.2)
-        saturated = self.color_filter(saturated, (51, 90), saturate=0.5, brighten=5.)
-        bgr = cv2.cvtColor(saturated, cv2.COLOR_HSV2BGR)
-
-        gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+        whitened = self._whiten_selected_colors(image)
+        gray = cv2.cvtColor(whitened, cv2.COLOR_BGR2GRAY)
 
         gaussian_size = (3, 3)
         gaussian_gray = cv2.GaussianBlur(gray, gaussian_size, 0)
@@ -19,6 +15,20 @@ class InitialFilter(IInitialFilter):
         equalized = cv2.equalizeHist(gaussian_gray)
 
         return equalized
+
+    BLUE_RANGE = (100, 135)
+    GREEN_RANGE = (51, 90)
+
+    def _whiten_selected_colors(self, image: np.ndarray) -> np.ndarray:
+        """
+        Make blue (EU country indicator) and green (electric car plate) more
+        similar to white, so that they will contribute more to the grayscale
+        image.
+        """
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image = self.color_filter(hsv, self.BLUE_RANGE, saturate=0.5, brighten=1.2)
+        image = self.color_filter(image, self.GREEN_RANGE, saturate=0.5, brighten=5.0)
+        return cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
 
     @staticmethod
     def color_filter(
