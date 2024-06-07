15a1a59fba5b0756747bbc312e4c393223e377ae
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-06-08 00:15:23 +0200

    move notebooks to platesmania

--- datasets/load.py
+++ datasets/load.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e4bde6612834a6c1e6be23aac6bb0985b5c00fde93c9a1a2587b15fc03650f7
-size 1422
+oid sha256:7822013e11edabce66f27e2bfd575725fb47be186a47b8215ce0af08862e4460
+size 2408
