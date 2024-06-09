9353041c7ae694507bf856caa96e3032dcbe357d
jwinter3 <117023023+jwinter3@users.noreply.github.com>
2024-06-09 22:16:28 +0200

    Filter some false positives

--- twm_proj/implementation/rect_classifier.py
+++ twm_proj/implementation/rect_classifier.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 from twm_proj.interface.rect_classifier import IRectClassifier, RectangleType
@@ -12,7 +13,7 @@ class RectClassifier(IRectClassifier):
 
     PLATE_ASPECTS = {typ: size[0] / size[1] for typ, size in PLATE_SIZES.items()}
 
-    EPSILON = 0.18
+    EPSILON = 0.24
 
     def classify(self, rect: np.ndarray) -> RectangleType:
         typ, _ = self.classify_with_differences(rect)
@@ -21,6 +22,21 @@ class RectClassifier(IRectClassifier):
     def classify_with_differences(
         self, rect: np.ndarray
     ) -> tuple[RectangleType, dict[RectangleType, float]]:
+        hsv_rect = cv2.cvtColor(rect, cv2.COLOR_BGR2HSV)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        _, _, k_means = cv2.kmeans(
+            hsv_rect.astype(np.float32).reshape((-1, 3)),
+            2,
+            None,
+            criteria,
+            10,
+            cv2.KMEANS_RANDOM_CENTERS,
+        )
+
+        k_means = np.sort(k_means, axis=0)
+        if (k_means[1] - k_means[0] < np.array([0, 0, 80])).any():
+            return RectangleType.NOT_PLATE, {}
+
         [height, width, *_] = rect.shape
         aspect = width / height
         relative_differences = {
